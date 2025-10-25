b5b3cbbe54e3562d7e423b10b0893992957279e9
grisha-git <donkeevgrisa@gmail.com>
2025-10-25 22:26:07 +0300

    homework with catching errors

--- f1.cpp
+++ f1.cpp
@@ -1,21 +1,44 @@
-include <iostream>
+#include <iostream>
+#include <limits>
+
+bool numbersNotCorrect(size_t a, size_t b, size_t c)
+{
+	bool p = a*a < a;
+	p = p || b*b < b;
+	p = p || c*c < c;
+	p = p || a*a > std::numeric_limits<size_t>::max() - b*b;
+	p = p || a*a > std::numeric_limits<size_t>::max() - c*c;
+	p = p || b*b > std::numeric_limits<size_t>::max() - c*c;
+	if (p){
+		return 1;
+	}
+	return 0;
+}
 bool isPyth(size_t a, size_t b, size_t c)
 {
-	bool p = a**a == b**b + c**c;
+	if (numbersNotCorrect(a, b, c)){
+		throw std::overflow_error("Too much");
+	}
+	bool p = a*a == b*b + c*c;
 	p = p || b*b == a*a + c*c;
 	p = p || c*c == a*a + b*b;
 	return p;
 }
-int main(){
+int main() {
 	using u_t = unsigned;
 	u_t a = 0, b = 0, c = 0;
-	std::cin >> c >> b;	
+	std::cin >> c >> b;
 	size_t count = 0;
 	while (std::cin >> a){
-		count += isPyth(a, b, c)? 1:0;
-		c = b;
-		b = a;
-	
+		try{
+			count += isPyth(a, b, c)? 1:0;
+			c = b;
+			b = a;
+		}
+		catch (const std::overflow_error &e){
+			std::cerr << e.what() << "\n";
+			return 2;
+		}
 	}
 	if (std::cin.eof()){
 		std::cout << count;
